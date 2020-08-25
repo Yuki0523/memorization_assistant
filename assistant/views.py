cb7f7b74a63ce04204b219e557ef9168652e63dd
輿水勇輝 <Yuki@MacBook-Pro.local>
2020-08-25 22:35:07 +0900

    ログイン後のトップページが大まかに完成した。

--- assistant/views.py
+++ assistant/views.py
@@ -2,4 +2,10 @@ from django.views import generic
 
 
 class TopView(generic.TemplateView):
-    template_name = 'top.html'
+    def get_template_names(self):
+        if self.request.user.is_authenticated:
+            template_name = 'top_log_in.html'
+        else:
+            template_name = 'top.html'
+
+        return [template_name]
